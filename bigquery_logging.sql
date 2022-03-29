28d3efb6a4aaebd1086f2be4a20d8f8a062ff2ba
Milad <milad@Milads-MacBook-Pro.local>
2022-03-29 12:40:30 +0430

    bigquery logging sample

---
+++ bigquery_logging.sql
@@ -0,0 +1,7 @@
+resource.type="bigquery_resource"
+resource.labels.project_id="business-intelligence-240201"
+protoPayload.serviceData.tableUpdateRequest.resource.tableName.tableId="campaign_expense_deposit"
+
+resource.type="bigquery_resource"
+resource.labels.project_id="business-intelligence-240201"
+protoPayload.serviceData.tableInsertRequest.resource.tableName.tableId="campaign_expense_deposit"
